a5d14ad010f367f8b69d2a958091519425f7e6e1
Giulia Zeni <giuli.zeni@gmail.com>
2023-02-21 16:23:31 +0100

    finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,37 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with; the create action on line 14 isn't enough
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path #tasks is the prefix I got from opening rails routes in the terminal
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
   # new path here!!!!!!! the order matters, :id should always be lower in the page
   get 'tasks/:id', to: 'tasks#show', as: 'task'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
